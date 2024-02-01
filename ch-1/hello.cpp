3f92aa4445d8a5a061934c1dc654d2b65d2e8dc2
prachisonani <lakhaniprachi8@gmail.com>
2024-02-01 12:34:02 +0530

    01/02/24

---
+++ ch-1/hello.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    int nub1;
+    int nub2;
+    
+    cout << "enter value of nub1" << endl;
+    cin >> nub1;
+
+    cout << "enter value of nub2" << endl;
+    cin >> nub2;
+    cout<<"sum is ="<<nub1+nub2;
+
+}
